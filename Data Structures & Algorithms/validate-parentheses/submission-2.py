ff1aa4273f388a53164efeda1d1c7b19c5eb9d4d
ScriptRed <122747380+ScriptRed@users.noreply.github.com>
2026-08-06 19:13:21 +0100

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parMap = {
+            "(":")",
+            "[":"]",
+            "{":"}",
+        }
+        heap = []
+        for c in s:
+            if c in parMap:
+                heap.append(parMap[c])
+            else:
+                if not heap:
+                    return False
+                if heap.pop() != c:
+                    return False
+        return len(heap) == 0
