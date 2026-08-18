73113a4264260923c67f9ab292feb98f74a8a018
ScriptRed <122747380+ScriptRed@users.noreply.github.com>
2026-08-19 00:56:43 +0100

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 0){
+            return 1;
+        }
+        vector<int> res(n+1);
+        res[0] = 1;
+        res[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            res[i] = res[i-1] + res[i-2];
+        }
+        return res[n];
+    }
+};
