8140bcee13794e19e4be260dc22e52948c308884
ScriptRed <122747380+ScriptRed@users.noreply.github.com>
2026-09-07 14:10:21 +0100

    Add: partition-labels - submission-5

---
+++ Data Structures & Algorithms/partition-labels/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        charToIndex = {}
+        res = []
+        for i in range(len(s)):
+            charToIndex[s[i]] = i
+        i = 0
+        while i < len(s):
+            if i < charToIndex[s[i]]:
+                start = i
+                reach = charToIndex[s[i]]
+                while i < reach:
+                    i += 1
+                    reach = max(reach,charToIndex[s[i]])
+                res.append(reach + 1 - start)
+                i+= 1
+
+            else:
+                res.append(1)
+                i += 1
+        return res       
